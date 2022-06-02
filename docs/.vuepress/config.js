77ef22792d1632b7d0b06e400d2e2622af6f5b9a
ccb1900 <guojianhang1992@outlook.com>
2022-06-02 09:05:32 +0800

    add menu

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,6 +1,42 @@
+const { defaultTheme } = require("@vuepress/theme-default");
+
 module.exports = {
-    lang: 'zh-CN',
-    title: '你好，微服务',
-    description: '一起走进微服务',
-    base: "/go-micro/"
-}
+  lang: "zh-CN",
+  title: "你好，微服务",
+  description: "一起走进微服务",
+  base: "/go-micro/",
+  theme: defaultTheme({
+    // 在这里进行配置
+    sidebar: [
+      // SidebarItem
+      {
+        text: "环境",
+        link: "/env.md",
+      },
+      {
+        text: "支付",
+        link: "/pay.md",
+      },
+      {
+        text: "开始",
+        link: "/",
+      },
+      {
+        text: "缓存",
+        link: "/cache.md",
+      },
+      {
+        text: "命令行",
+        link: "/console.md",
+      },
+      {
+        text: "熔断",
+        link: "/console.md",
+      },
+      {
+        text: "限流",
+        link: "/console.md",
+      },
+    ],
+  }),
+};
--- readme.md
+++ readme.md
@@ -5,3 +5,11 @@
 本项目提供了一个环境，用于测试。详情见 docker-compose.yml。
 
 测试覆盖率。
+
+文档离线使用
+
+克隆本项目
+
+yarn
+
+yarn docs:dev
